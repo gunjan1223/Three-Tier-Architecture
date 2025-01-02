14dac084b57e5981869c91876da36d345568bb03
Gunjan Tambekar <118653409+gunjan1223@users.noreply.github.com>
2025-01-02 17:55:20 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'mytierdbproject.chioai2omnqy.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Gunjan12345',
+    DB_DATABASE : 'webappdb'
+});
